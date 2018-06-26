3e1b9ff9424fadd0c8110059bc5dec4fd6be893e
yebuzhen <791544415@qq.com>
2018-06-26 16:46:58 +0100

    Admin homepage stylish

--- student.php
+++ student.php
@@ -16,12 +16,12 @@ if (isset($_GET['logout'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Student Request</title>
+    <title>Student</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link rel="stylesheet" href="css/student.css">
+    <link rel="stylesheet" href="css/homepage.css">
 </head>
 
 <body>
