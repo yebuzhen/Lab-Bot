580e26c0ff3381b6704a0fd635e9dda33f19db3e
yebuzhen <791544415@qq.com>
2018-06-26 15:04:52 +0100

    Login interface update

--- adminLogin.php
+++ adminLogin.php
@@ -8,20 +8,28 @@ session_start();
     <meta charset="UTF-8">
     <title>Student Login</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 
 <body>
-<p id="info">Login Please (for assistants)</p>
-<form action="onAdminLogin.php" method="post">
-    <input type="email" id="email" name="email" placeholder="Email"><br>
-    <input type="password" id="password" name="password" placeholder="Password"><br>
-    <input type="submit" value="Login">
-</form>
-<script>
 
-
-</script>
+    <div class="wrapper">
+        <form class="login" action="onAdminLogin.php" method="post">
+            <p class="title">Log in (for assistants)</p>
+            <input type="email" id="email" name="email" placeholder="Email" autofocus/>
+            <i class="fa fa-user"></i>
+            <input type="password" id="password" name="password" placeholder="Password" />
+            <i class="fa fa-key"></i>
+            <button type="submit">
+                <i class="spinner"></i>
+                <span class="state">Log in</span>
+            </button>
+        </form>
+    </div>
 
 </body>
 
